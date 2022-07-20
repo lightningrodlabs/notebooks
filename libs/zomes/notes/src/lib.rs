09989ad69cca3707dbe749a9357a20376f7b8438
Wesley Finck <wesleyfinck@gmail.com>
2022-07-20 14:30:07 +0200

    create backlinks between notes given an existing note title

--- libs/zomes/notes/src/lib.rs
+++ libs/zomes/notes/src/lib.rs
@@ -26,16 +26,30 @@ pub struct CreateNoteInput {
     pub timestamp: Timestamp,
     pub syn_dna_hash: DnaHashB64,
 }
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct NoteBacklinks {
+    pub links_to: BTreeMap<String, EntryHashB64>,
+    pub linked_from: BTreeMap<String, EntryHashB64>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct UpdateNoteBacklinksInput {
+    pub note: EntryHashB64,
+    pub link_titles: Vec<String>,
+}
 
 #[hdk_extern]
 pub fn create_new_note(input: CreateNoteInput) -> ExternResult<EntryHashB64> {
     let creator = agent_info()?.agent_latest_pubkey;
 
+    // TODO: check if any other note links to this one, if it does, add it to backlink
+    // for now assume cannot start with backlinks
     let note = Note {
         creator: creator.into(),
         timestamp: input.timestamp,
         syn_dna_hash: input.syn_dna_hash,
-        title: input.title,
+        title: input.title.clone(),
     };
 
     create_entry(&note)?;
@@ -46,6 +60,12 @@ pub fn create_new_note(input: CreateNoteInput) -> ExternResult<EntryHashB64> {
 
     create_link(
         path.path_entry_hash()?,
+        hash.clone(),
+        HdkLinkType::Any,
+        (),
+    )?;
+    create_link(
+        title_path(input.title).path_entry_hash()?,
         hash.clone(),
         HdkLinkType::Any,
         (),
@@ -90,6 +110,100 @@ pub fn get_all_notes(_: ()) -> ExternResult<BTreeMap<EntryHashB64, Note>> {
     Ok(notes_map)
 }
 
+
+#[hdk_extern]
+pub fn update_note_backlinks(input: UpdateNoteBacklinksInput) -> ExternResult<()> {
+    // - figures out which links to delete, and which links to add  
+    // - gets all links of type `linksTo`, maps over all then, checking if the tag name matches the link names, if it doesn't delete (delete backlink as well)  
+    // - for each title in link_titles, check if matches a tag name, if not, create a new link
+    // - return the notes backlinks?
+    let links = get_links(EntryHash::from(input.note.clone()), None)?;
+    // find overlaps
+    for title in input.link_titles {
+        match links.iter().find(|link| link.clone().tag.eq(&links_to_tag(title.clone()))) {
+            // link exists
+            Some(_) => (),
+            // link doesn't exist, so create it
+            None => add_backlink(input.note.clone().into(), title)?,
+        }
+    }
+    Ok(())
+}
 fn all_notes_path() -> Path {
     Path::from("all_notes")
+}
+
+fn title_path(title: String) -> Path {
+    Path::from(title)
+}
+
+fn links_to_tag(title: String) -> LinkTag {
+    LinkTag::new(String::from(format!("links_to_{}", title)))
+}
+fn linked_from_tag(title: String) -> LinkTag {
+    LinkTag::new(String::from(format!("linked_from_{}", title)))
+}
+
+fn add_backlink(base_note: EntryHash, target_note_title: String) -> ExternResult<()> {
+    // get note by title
+    match get_note_by_title(target_note_title.clone())? {
+        Some(target_note) => {
+            let target_note_hash = hash_entry(target_note)?;
+            create_link(
+                base_note.clone(),
+                target_note_hash.clone(),
+                HdkLinkType::Any,
+                links_to_tag(target_note_title),
+            )?;
+            create_link(
+                target_note_hash,
+                base_note.clone(),
+                HdkLinkType::Any,
+                linked_from_tag(get_note(base_note)?.title),
+            )?;
+            ()
+        },
+        // note doesn't exist
+        None => (),
+    };
+    Ok(())
+}
+
+#[hdk_extern]
+pub fn get_note_by_title(title: String) -> ExternResult<Option<Note>> {
+    let mut links = get_links(
+        title_path(title).path_entry_hash()?,
+        None,
+    )?;
+    match links.pop() {
+        Some(link) => { 
+            match get::<EntryHash>(link.target.into(), GetOptions::default())? {
+                Some(element) => { 
+                    let note: Note = element.entry().to_app_option()?.ok_or(WasmError::Guest(String::from("Malformed note")))?;
+                    Ok(Some(note))
+                },
+                None => Ok(None),
+            }
+        },
+        None => Ok(None),
+    }
+}
+
+fn get_note(entry_hash: EntryHash) -> ExternResult<Note> {
+    match get(entry_hash, GetOptions::default())? {
+        Some(element) => {
+            let note: Note = element.entry().to_app_option()?.ok_or(WasmError::Guest(String::from("malformed note")))?;
+            Ok(note)
+        },
+        None => Err(WasmError::Guest(String::from("unable to resolve note from entry hash"))),
+    }
+}
+
+// pub fn get_note_links(note_hash: EntryHashB64) -> ExternResult<BTreeMap<String, EntryHashB64>> {
+#[hdk_extern]
+pub fn get_note_links(note_hash: EntryHashB64) -> ExternResult<Vec<Link>> {
+    get_links::<EntryHash>(
+        note_hash.into(),
+        None,
+    )
 }
--- libs/zomes/notes/tests/create_and_get.rs
+++ libs/zomes/notes/tests/create_and_get.rs
@@ -7,7 +7,7 @@ use hdk::prelude::Timestamp;
 use hdk::prelude::*;
 use holochain::test_utils::consistency_10s;
 use holochain::{conductor::config::ConductorConfig, sweettest::*};
-use notes::{CreateNoteInput, Note};
+use notes::{CreateNoteInput, Note, UpdateNoteBacklinksInput};
 
 #[tokio::test(flavor = "multi_thread")]
 async fn create_and_get() {
@@ -32,13 +32,14 @@ async fn create_and_get() {
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
 
+    let note_title_1 = String::from("new note");
     let create_note_input = CreateNoteInput {
-        title: String::from("new note"),
+        title: note_title_1.clone(),
         timestamp: Timestamp::from_micros(since_the_epoch.as_micros() as i64),
         syn_dna_hash: fixt!(DnaHashB64),
     };
 
-    let _note_hash: EntryHashB64 = conductors[0]
+    let first_note_hash: EntryHashB64 = conductors[0]
         .call(&alice_zome, "create_new_note", create_note_input)
         .await;
 
@@ -51,4 +52,47 @@ async fn create_and_get() {
 
     let note = all_notes.values().last().unwrap();
     assert_eq!(note.title, String::from("new note"));
+
+    let note_by_title: Option<Note> = conductors[1].call(&bob_zome, "get_note_by_title", note_title_1.clone()).await;
+    assert_eq!(note_by_title.unwrap().title, note_title_1.clone());
+
+    let note_title_2 = String::from("new note 2");
+    let create_note_input_2 = CreateNoteInput {
+        title: note_title_2.clone(),
+        timestamp: Timestamp::from_micros(since_the_epoch.as_micros() as i64),
+        syn_dna_hash: fixt!(DnaHashB64),
+    };
+
+    let second_note_hash: EntryHashB64 = conductors[0]
+        .call(&alice_zome, "create_new_note", create_note_input_2)
+        .await;
+
+    consistency_10s(&[&alice, &bobbo]).await;
+    let update_note_backlinks_input = UpdateNoteBacklinksInput {
+        note: first_note_hash.clone(),
+        link_titles: vec![String::from("new note 2")],
+    };
+    let non_existant_notes = UpdateNoteBacklinksInput {
+        note: first_note_hash.clone(),
+        link_titles: vec![String::from("blah")],
+    };
+    let _result: () = conductors[0]
+        .call(&alice_zome, "update_note_backlinks", update_note_backlinks_input)
+        .await;
+    // should not create a backlink since the note doesn't exist
+    let _result: () = conductors[0]
+        .call(&alice_zome, "update_note_backlinks", non_existant_notes)
+        .await;
+
+    let note_links: Vec<Link> = conductors[0]
+        .call(&alice_zome, "get_note_links", first_note_hash.clone())
+        .await;
+    let linked_note_links: Vec<Link> = conductors[0]
+        .call(&alice_zome, "get_note_links", second_note_hash.clone())
+        .await;
+
+    assert_eq!(note_links.len(),1);
+    assert_eq!(EntryHash::from(note_links.clone().pop().unwrap().target), EntryHash::from(second_note_hash));
+    assert_eq!(linked_note_links.len(),1);
+    assert_eq!(EntryHash::from(linked_note_links.clone().pop().unwrap().target), EntryHash::from(first_note_hash));
 }
