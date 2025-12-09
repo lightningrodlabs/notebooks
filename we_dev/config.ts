3fa59615ec7bfc1e3eda2aef381101a28b9b67eb
Leo <leobensman@gmail.com>
2025-12-09 15:18:21 +0100

    Update packages and fix creatable

--- we_dev/config.ts
+++ we_dev/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@theweave/cli';
 export default defineConfig({
   toolCurations: [
     {
-      url: 'https://raw.githubusercontent.com/lightningrodlabs/weave-tool-curation/refs/heads/test-0.13/0.13/lists/curations-0.13.json',
+      url: 'https://raw.githubusercontent.com/lightningrodlabs/weave-tool-curation/refs/heads/test-0.15/0.15/lists/curations-0.15.json',
       useLists: ['default'],
     },
   ],
@@ -50,12 +50,12 @@ export default defineConfig({
         //   registeringAgent: 1,
         //   joiningAgents: [2],
         // },
-        {
-          name: 'kando',
-          instanceName: 'kando',
-          registeringAgent: 1,
-          joiningAgents: [2],
-        },
+        // {
+        //   name: 'kando',
+        //   instanceName: 'kando',
+        //   registeringAgent: 1,
+        //   joiningAgents: [2],
+        // },
       ],
     },
   ],
@@ -87,18 +87,18 @@ export default defineConfig({
   //         url: "https://github.com/holochain-apps/gamez/releases/download/v0.4.2/gamez.webhapp"
   //       },
   //     },
-      {
-      name: 'kando',
-      subtitle: 'kanban boards',
-      description: 'Real-time kanban based on syn',
-      icon: {
-        type: 'https',
-        url: 'https://raw.githubusercontent.com/holochain-apps/kando/main/we_dev/kando_icon.png',
-      },
-      source: {
-        type: 'https',
-        url: 'https://github.com/holochain-apps/kando/releases/download/v0.13.0-rc.0/kando.webhapp',
-      },
-    },
+      // {
+      // name: 'kando',
+      // subtitle: 'kanban boards',
+      // description: 'Real-time kanban based on syn',
+      // icon: {
+      //   type: 'https',
+      //   url: 'https://raw.githubusercontent.com/holochain-apps/kando/main/we_dev/kando_icon.png',
+      // },
+      // source: {
+      //   type: 'https',
+      //   url: 'https://github.com/holochain-apps/kando/releases/download/v0.13.0-rc.0/kando.webhapp',
+      // },
+    // },
  ],
 });
